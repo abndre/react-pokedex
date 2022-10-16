bf7a991300e61c75e88a8813fbe09335106d48bf
andrezio <andrezio@live.com>
2022-10-16 18:18:29 -0300

    Updates

---
+++ precache-manifest.d44e3ab480e29449898973046eb69f4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "09b2b612237c86d7e7fd",
+    "url": "/react-pokedex/static/css/main.15df645a.chunk.css"
+  },
+  {
+    "revision": "09b2b612237c86d7e7fd",
+    "url": "/react-pokedex/static/js/main.09b2b612.chunk.js"
+  },
+  {
+    "revision": "68d85c9b0ba336868cc8",
+    "url": "/react-pokedex/static/js/1.68d85c9b.chunk.js"
+  },
+  {
+    "revision": "9f1dc7ae626253023241",
+    "url": "/react-pokedex/static/js/2.9f1dc7ae.chunk.js"
+  },
+  {
+    "revision": "d24f7837f46c94e99f57",
+    "url": "/react-pokedex/static/js/runtime~main.d24f7837.js"
+  },
+  {
+    "revision": "2b4798f5c303bb0d9001413af970a22b",
+    "url": "/react-pokedex/static/media/pokeball.2b4798f5.svg"
+  },
+  {
+    "revision": "9081ee405704787bd8a5aacbc1c1eecf",
+    "url": "/react-pokedex/index.html"
+  }
+];
