69393f3aaac3536857bd5db23990e0d9b130c4ab
andrezio <andrezio@live.com>
2022-10-15 21:08:00 -0300

    form para coletar pokemon com axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { type } from "@testing-library/user-event/dist/type";
+import React, {useState} from "react";
+import api from "./components/api";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [pokemon, setPokemon] = useState(null);
+  const [error, setError] = useState(null);
+  const [typedPokemon, setTypedPokemon] = useState('');
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleChange = (event) => {
+    setTypedPokemon(event.target.value.toLowerCase());
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (!typedPokemon){
+      return;
+    }
+    setIsLoading(true);
+    try {
+      const response = await api.get(`/pokemon/${typedPokemon}`);
+      setPokemon(response.data);
+      setError(null)
+      setIsLoading(false);
+    } catch(error){
+      setError("Pokemon nao encontrado")
+      setIsLoading(false);
+      setPokemon(null);
+    }
+  };
+
+   return (
+    <div>
+      <h1>SEJA BEM BINDO A POKEDEX!</h1>
+      <p>Digite o nome ou id do pokemon</p>
+      <form onSubmit={handleSubmit}>
+        <input 
+          placeholder="Numero ou Nome do pokemon"
+          value={typedPokemon}
+          onChange={handleChange}>
+        </input>
+        <button type="submit">
+          {
+            isLoading ? (
+              <span>carregando...</span>
+            ) : (
+              <>
+              Buscar
+              </>
+            )
+          }
+        </button>
+      </form>
+      {pokemon && (
+        <div>
+          <h2>HELLO</h2> 
+          <h3>{pokemon.name}</h3>
+        </div>
+      )}
     </div>
   );
 }
