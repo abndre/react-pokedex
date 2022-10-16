9d6c4c15d390cbd492716224b4062d878739eb0c
andrezio <andrezio@live.com>
2022-10-16 07:43:30 -0300

    Atualizado componentes para renderizar a pokedex

--- src/App.js
+++ src/App.js
@@ -1,65 +1,14 @@
-import { type } from "@testing-library/user-event/dist/type";
-import React, {useState} from "react";
-import api from "./components/api";
+import React from "react";
+import Pokedex from "./components/Pokedex";
+
 
 function App() {
-  const [pokemon, setPokemon] = useState(null);
-  const [error, setError] = useState(null);
-  const [typedPokemon, setTypedPokemon] = useState('');
-  const [isLoading, setIsLoading] = useState(false)
-
-  const handleChange = (event) => {
-    setTypedPokemon(event.target.value.toLowerCase());
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    if (!typedPokemon){
-      return;
-    }
-    setIsLoading(true);
-    try {
-      const response = await api.get(`/pokemon/${typedPokemon}`);
-      setPokemon(response.data);
-      setError(null)
-      setIsLoading(false);
-    } catch(error){
-      setError("Pokemon nao encontrado")
-      setIsLoading(false);
-      setPokemon(null);
-    }
-  };
-
-   return (
+  return(
     <div>
-      <h1>SEJA BEM BINDO A POKEDEX!</h1>
-      <p>Digite o nome ou id do pokemon</p>
-      <form onSubmit={handleSubmit}>
-        <input 
-          placeholder="Numero ou Nome do pokemon"
-          value={typedPokemon}
-          onChange={handleChange}>
-        </input>
-        <button type="submit">
-          {
-            isLoading ? (
-              <span>carregando...</span>
-            ) : (
-              <>
-              Buscar
-              </>
-            )
-          }
-        </button>
-      </form>
-      {pokemon && (
-        <div>
-          <h2>HELLO</h2> 
-          <h3>{pokemon.name}</h3>
-        </div>
-      )}
+      <Pokedex></Pokedex>
     </div>
-  );
+  )
+
 }
 
 export default App;
---
+++ src/components/Pokedex.js
@@ -0,0 +1,81 @@
+import React, {useState} from "react";
+import api from "./api";
+
+
+function Pokedex() {
+    const [pokemon, setPokemon] = useState(null);
+    const [error, setError] = useState(null);
+    const [typedPokemon, setTypedPokemon] = useState('');
+    const [isLoading, setIsLoading] = useState(false)
+  
+    const handleChange = (event) => {
+      setTypedPokemon(event.target.value.toLowerCase());
+    };
+  
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+      if (!typedPokemon){
+        return;
+      }
+      setIsLoading(true);
+      try {
+        const response = await api.get(`/pokemon/${typedPokemon}`);
+        setPokemon(response.data);
+        setError(null)
+        setIsLoading(false);
+      } catch(error){
+        setError("Pokemon nao encontrado")
+        setIsLoading(false);
+        setPokemon(null);
+      }
+    };
+  
+     return (
+      <div className="pokedex">
+        <h1>SEJA BEM BINDO A POKEDEX!</h1>
+        <p>Digite o nome ou id do pokemon</p>
+        <form onSubmit={handleSubmit}>
+          <input 
+            placeholder="Numero ou Nome do pokemon"
+            value={typedPokemon}
+            onChange={handleChange}>
+          </input>
+          <button type="submit">
+            {
+              isLoading ? (
+                <span>carregando...</span>
+              ) : (
+                <>
+                Buscar
+                </>
+              )
+            }
+          </button>
+        </form>
+        {pokemon && (
+          <>
+            <div key={pokemon.id}>
+             <h2>{pokemon.name}</h2>
+             <img src={pokemon.sprites['front_default']} alt={pokemon.name} />
+           </div>
+           <div>
+               <span>
+                  <strong>Altura: </strong> {pokemon.height * 10}cm
+               </span>
+               <span>
+                  <strong>Peso: </strong> {pokemon.weight / 10} kg
+               </span>
+               <span>
+                  <strong>Tipo Principal: </strong> {pokemon.types[0].type.name}
+               </span>
+               <span>
+                  <strong>ID: </strong> {pokemon.id}
+               </span>
+            </div>
+          </>  
+        )}
+          </div>
+    )
+  }
+
+export default Pokedex
